3ca3c358bf8c5cc35acb7d66d2848c81a0cf6423
VitalikSakovets <sakowez123@gmail.com>
2023-05-08 13:19:22 +0300

    Modefile variant 6

--- realese.py
+++ realese.py
@@ -5,20 +5,8 @@
 #Назар
 
 #Віталій
-type = input("Введіть тип даних (char, int, str, float):")
-fileobj = open('df.txt')
-for i in fileobj:
-    try:
-        if(type == "int"):
-            print(int(i))
-        elif(type == "str"):
-            print(str(i))
-        elif ((type == "char") & (len(i.strip()) == 1)):
-            print(i)
-        elif ((type == "float")&((float(i)%1) > 0)):
-            print(i)
-    except:
-        pass
+import ModulesWorkFiles.work_file_6 as vs
+vs.task6()
 
 #Сергій
 
