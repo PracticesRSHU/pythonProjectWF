814ab3ab5a0cf2011e93602ecb0ed5e8c9d5263d
petro8451 <petro.shidlovskiy@gmail.com>
2023-06-08 22:46:28 +0300

    Add file

--- ModulesWorkFiles/Work file 7.py
+++ ModulesWorkFiles/Work file 7.py
@@ -1,2 +1,19 @@
 #Hello
 #555
+def swap_first_last_words(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    swapped_lines = []
+    for line in lines:
+        words = line.strip().split()
+        if len(words) >= 2:
+            words[0], words[-1] = words[-1], words[0]
+        swapped_line = ' '.join(words)
+        swapped_lines.append(swapped_line)
+
+    with open(filename, 'w') as file:
+        file.writelines(swapped_lines)
+
+# Приклад виклику функції:
+swap_first_last_words('input.txt')
